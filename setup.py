e2a74f05dc6874905a636fa9088287567f06d919
email <rrizwan1998@gmail.com>
2023-07-21 23:24:02 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = '3D_Printing',
+    version= '0.0.0',
+    author= 'Raza Rizwan',
+    author_email= 'rrizwan1998@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
